2df9e4996d953415f20a3357f3f27e0485fb9de4
Ricardo Alvarez <ricardo.alvarezr94@gmail.com>
2021-11-20 10:20:29 -0300

    Se agregan los datos del alumno ALVAREZ, Ricardo

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s RICARDO_ALVAREZ = {
+    .apellidos = "ALVAREZ RUCH",
+    .nombres = "Ricardo Arturo",
+    .documento = "95.786.583",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &RICARDO_ALVAREZ
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
